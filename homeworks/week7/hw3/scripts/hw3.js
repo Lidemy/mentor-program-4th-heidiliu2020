7d5bbeff20288666bf4e2edc0187b386f5b052cf
Heidi <liuheidi2020@gmail.com>
2020-09-03 21:42:01 +0800

    week7 modified

--- homeworks/week7/hw3/scripts/hw3.js
+++ homeworks/week7/hw3/scripts/hw3.js
@@ -5,35 +5,52 @@ document.querySelector('.list__container').addEventListener('click', function (e
     e.target.parentNode.classList.toggle('item-finished');
     e.target.parentNode.querySelector('.item-context').classList.toggle('item-context-finished');
   }
-  // 刪除 item
+  // 刪除 todo
   if (e.target.classList.contains('btn-deleted')) {
     e.target.parentNode.classList.add('item-deleted');
   }
 });
 
-// 新增 item：監聽鍵盤事件 keypress
+// 新增 todo：監聽鍵盤事件 keypress
 document.querySelector('.list__input').addEventListener('keypress', function (e) {
-  const input = document.querySelector('.list__input');
-  const inputValue = input.value;
-
   // Enter 對應鍵盤代碼：13
   if (e.which === 13) {
-    // 若輸出值為空字串，彈出警告視窗；沒有則輸出 value
-    if (inputValue === '') {
-      alert('Please write something here!');
-    } else {
-      const newItem = document.createElement('li');
-      newItem.classList.add('list__item');
-      newItem.innerHTML = `
+    addTodos();
+  }
+});
+
+// 新增 todo：監聽點擊事件 click
+document.querySelector('.add-btn').addEventListener('click', function (e) {
+  addTodos();
+})
+
+function addTodos() {
+  const input = document.querySelector('.list__input');
+  const inputValue = input.value;
+  // 若輸出值為空字串，彈出警告視窗；沒有則輸出 value
+  if (inputValue === '') {
+    alert('Please write something here!');
+  } else {
+    const newItem = document.createElement('li');
+    newItem.classList.add('list__item');
+    newItem.innerHTML = `
         <input class="item-checkbox" type="checkbox">
-        <p class="item-context"></p>
+        <p class="item-context">${escapeHtml(inputValue)}</p>
         <button class="btn-deleted"></button>
       `;
 
-      newItem.querySelector('.item-context').innerText = inputValue;
-      document.querySelector('.list__container').appendChild(newItem);
-    }
-    // 輸出完成後清空 input 框
-    input.value = '';
+    document.querySelector('.list__container').appendChild(newItem);
   }
-});
+  // 輸出完成後清空 input 框
+  input.value = '';
+}
+
+// 跳脫函式：處理特殊字元
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
