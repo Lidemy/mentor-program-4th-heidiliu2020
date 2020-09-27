12c6735a355947f7109ed097c2d57068010e1882
Heidi <liuheidi2020@gmail.com>
2020-09-27 21:24:31 +0800

    escape update

--- homeworks/week11/hw1/admin.php
+++ homeworks/week11/hw1/admin.php
@@ -42,59 +42,61 @@
     注意！本站為練習用網站，註冊時請勿使用任何真實的帳號或密碼。
   </header>
 
-  <div class="wrapper">
-    <main class="board admin-page">　
-      <div class ="board__header">
-        <h1 class="board__tittle">後台管理</h1>
-        <div class="board__btn-block">
-          <a class="board__btn" href="index.php">返回</a>  
+  <div class="bg">
+    <div class="wrapper">
+      <main class="board admin-page">　
+        <div class ="board__header">
+          <h1 class="board__tittle">後台管理</h1>
+          <div class="board__btn-block">
+            <a class="board__btn" href="index.php">返回</a>  
+          </div>
         </div>
-      </div>
-      <section>
-        <table class="admin__table"> 
-          <tr>
-            <th>id</th>
-            <th>nickname</th>
-            <th>username</th>
-            <th>role</th>
-          </tr>
-        <?php
-          // 讀取資料庫資料
-          while($row = $result->fetch_assoc()) {
-        ?>
-          <tr>
-            <td><?php echo escape($row['id']); ?></td>
-            <td><?php echo escape($row['nickname']); ?></td>
-            <td><?php echo escape($row['username']); ?></td>
-            <td>
-              <?php
-              if ($row["role"] === "Admin") {
-                echo "管理員";
-              } else if ($row["role"] === "Normal") {
-                echo "一般會員";
-              } else {
-                echo "已停權會員";
-              }
-              ?>
+        <section>
+          <table class="admin__table"> 
+            <tr>
+              <th>id</th>
+              <th>nickname</th>
+              <th>username</th>
+              <th>role</th>
+            </tr>
+          <?php
+            // 讀取資料庫資料
+            while($row = $result->fetch_assoc()) {
+          ?>
+            <tr>
+              <td><?php echo escape($row['id']); ?></td>
+              <td><?php echo escape($row['nickname']); ?></td>
+              <td><?php echo escape($row['username']); ?></td>
+              <td>
+                <?php
+                if ($row["role"] === "Admin") {
+                  echo "管理員";
+                } else if ($row["role"] === "Normal") {
+                  echo "一般會員";
+                } else {
+                  echo "已停權會員";
+                }
+                ?>
 
-              <form class="admin__table-select" method="POST" action="handle_update_role.php?id=<?php echo escape($row['id']) ?>">
-                <!-- select / option 下拉式選單 -->
-                <select name="role">
-                  <!-- option 預設選項；不能被點選；不會出現在下拉框中 -->
-                  <option selected disabled hidden >選擇身分</option>
-                  <option value="Admin">管理員</option>
-                  <option value="Normal">一般會員</option>
-                  <option value="Banned">已停權會員</option>
-                </select>
-                <input type="submit" value="提交" class="table-select__btn">
-              </form>
-            </td>
-          </tr>
-        <?php } ?>
-        </table>
-      </section>
+                <form class="admin__table-select" method="POST" action="handle_update_role.php?id=<?php echo escape($row['id']) ?>">
+                  <!-- select / option 下拉式選單 -->
+                  <select name="role">
+                    <!-- option 預設選項；不能被點選；不會出現在下拉框中 -->
+                    <option selected disabled hidden >選擇身分</option>
+                    <option value="Admin">管理員</option>
+                    <option value="Normal">一般會員</option>
+                    <option value="Banned">已停權會員</option>
+                  </select>
+                  <input type="submit" value="提交" class="table-select__btn">
+                </form>
+              </td>
+            </tr>
+          <?php } ?>
+          </table>
+        </section>
 
-    </main>
+      </main>
+    </div>
   </div>
 
 </body>
